a9111be29f47b70f9ef93dad5df31c6a31c7eb27
YoungCheon <kakao_story@naver.com>
2022-05-06 16:39:17 +0900

    5/6 풀이

---
+++ 2022/05/11051.py
@@ -0,0 +1,3 @@
+from math import factorial
+n,k = map(int,input().split())
+print(factorial(n)/(factorial(k)*factorial(n-k))%10007)
--- 2022/05/README.md
+++ 2022/05/README.md
@@ -1,2 +1,3 @@
 # boj-everyday
 ## 2022/05
+이항 계수2 https://www.acmicpc.net/problem/11051
