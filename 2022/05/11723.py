48be1350c253dd32bfa864bec8b4a21786a2f044
YoungCheon <kakao_story@naver.com>
2022-05-09 17:28:05 +0900

    5/9

--- 2022/05/11723.py
+++ 2022/05/11723.py
@@ -4,16 +4,16 @@ for _ in range(int(input())):
     x = input().rstrip().split()
     command = x[0]
     if command == 'add':
-        s.add(x[1])
+        s.add(int(x[1]))
     elif command == 'remove':
-        s.discard(x[1])
+        s.discard(int(x[1]))
     elif command == 'check':
-        print((x[1] in s)+0)
+        print((int(x[1]) in s)+0)
     elif command == 'toggle':
-        if x[1] in s:
-            s.discard(x[1])
+        if int(x[1]) in s:
+            s.discard(int(x[1]))
         else:
-            s.add(x[1])
+            s.add(int(x[1]))
     elif command == 'all':
         s = set(range(1,21))
     else:
