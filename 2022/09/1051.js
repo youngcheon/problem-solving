2936e44ba0804f872f59e2ed341543e0d4202cf1
YoungCheon <kakao_story@naver.com>
2022-09-06 23:09:56 +0900

    9/6

---
+++ 2022/09/1051.js
@@ -0,0 +1,16 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const [N, M] = input.shift().split(" ").map(Number);
+const board = input.map((e) => [...e].map(Number));
+
+let maxValue = 0;
+for (let y = 0; y < N; y++) {
+    for (let x = 0; x < M; x++) {
+        for (let i = 0; i <= 50; i++) {
+            if (y + i >= N || x + i >= M) break;
+            if (board[y][x] === board[y][x + i] && board[y][x] === board[y + i][x] && board[y][x] === board[y + i][x + i]) {
+                maxValue = Math.max(maxValue, i);
+            }
+        }
+    }
+}
+console.log((maxValue + 1) ** 2);
