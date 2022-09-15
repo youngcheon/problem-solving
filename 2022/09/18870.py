af1875aa633f18d3e3b31f4eacd0f638733b875d
YoungCheon <kakao_story@naver.com>
2022-09-15 21:44:10 +0900

    9/15

---
+++ 2022/09/18870.py
@@ -0,0 +1,8 @@
+input = __import__('sys').stdin.readline
+N = input()
+numbers = list(map(int, input().split()))
+
+SortedNumbers = sorted(set(numbers))
+hashtable = {e : i for i, e in enumerate(SortedNumbers)}
+
+print(' '.join(map(lambda x : str(hashtable[x]), numbers)))
